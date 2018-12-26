09c51cba299591844de2c5a2f98b83f7dc78ca71
Z <kerenyi.nagy.zoltan@gmail.com>
2018-12-26 10:36:57 +0100

    Update next5minCRON.sh

--- bash/next5minCRON.sh
+++ bash/next5minCRON.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # how many seconds have left until next 5-minute-cyclic cron job
-set -x
+# needs further testing
 
+set -x
 date +"%H:%M:%S" >>test2.txt
 
 CURRENT_MINUTES=$(date +'%M' | cut -c 2-)
@@ -12,10 +13,9 @@ if [ "$CURRENT_MINUTES" -eq 0 ] && [ "$CURRENT_SECONDS" -eq 0 ]; then
 	exit 0
 elif [ "$CURRENT_MINUTES" -eq 5 ] && [ "$CURRENT_SECONDS" -eq 0 ]; then
 	exit 0
-elif [ "$CURRENT_MINUTES" -gt 0 ] && [ "$CURRENT_MINUTES" -lt 5 ];then
-	R=$((300-(CURRENT_MINUTES*60+CURRENT_SECONDS)))
-	
-elif [ "$CURRENT_MINUTES" -gt 5 ] && [ "$CURRENT_MINUTES" -lt 10 ];then
+elif [ "$CURRENT_MINUTES" -gt 0 ] && [ "$CURRENT_MINUTES" -lt 5 ]; then
+	R=$((300-(CURRENT_MINUTES*60+CURRENT_SECONDS)))	
+elif [ "$CURRENT_MINUTES" -gt 5 ] && [ "$CURRENT_MINUTES" -lt 10 ]; then
 	R=$((600-(CURRENT_MINUTES*60+CURRENT_SECONDS)))	
 else
 	exit 1
